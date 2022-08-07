ecc3dd32222d73edc96abf1d2a54c57c517217a2
Ewwgen <110337565+Ewwgen@users.noreply.github.com>
2022-08-07 15:16:22 +0300

    Update 03_task_dict.py

--- Module3/practice/dict/03_task_dict.py
+++ Module3/practice/dict/03_task_dict.py
@@ -29,6 +29,10 @@ staff = [
 # Вычислите:
 print("Имя и Фамилию сотрудника с самой высокой зарплатой:")
 
+for i in range(len(staff)):
+    if int(staff[i]['salary'])>max():
+        max=staff[i]['salary']
+
 # TODO: your code here
 
 print("Имя и Фамилию сотрудника с самой низкой зарплатой:")
@@ -41,8 +45,14 @@ print("Среднеарифметическую зарплату всех сот
 
 print("Количество однофамильцев в организации")
 
+for person in staff:
+    count=0
+    for another_person in staff:
+        if person['name']==another_person['name']
+            count+=1
+        if count>1:
+            print(person['name'])
+
 # TODO: your code here
 
 print("*Список всех сотрудников(Имя и Фамилию) в порядке возрастания их зарплаты")
-
-# TODO: your code here
