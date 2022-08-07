4f03eeb822474f5cc14b4bb7c1cfcedfedd39cf0
Ewwgen <110337565+Ewwgen@users.noreply.github.com>
2022-08-07 16:32:18 +0300

    Update 01_my_abs.py

--- Module4/practice/funcs_part0/01_my_abs.py
+++ Module4/practice/funcs_part0/01_my_abs.py
@@ -1,7 +1,10 @@
-def my_abs(...):
-    ...
-    return ...
+def my_abs(a):
+    if a > 0:
+        return a
+    else:
+        return(a*-1)
 
+print(my_abs(0))
 
 print(my_abs(-5))
 print(my_abs(5))
