b17e9795a3cb406e8c6cdf4e786f1179114174c0
Ewwgen <110337565+Ewwgen@users.noreply.github.com>
2022-08-07 15:03:42 +0300

    Update 02_task_dict.py

--- Module3/practice/dict/02_task_dict.py
+++ Module3/practice/dict/02_task_dict.py
@@ -4,6 +4,12 @@
 keys = ['name', 'surname', 'age', 'rate']
 values = ['Петр', 'Первый', 42, 1300]
 
+dict1={}
+
+for i in range(len(keys)):
+    dict1[keys[i]]=values[i]
+
+print(dict1)
 # TODO: your code here
 # Нужно получить словарь:
 # {'name': 'Петр', 'surname': 'Первый', 'age': 42, 'rate': 1300}
