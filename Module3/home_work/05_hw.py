c7a628a56d573b2fe162706bf559b3c3a90812a9
Ewwgen <110337565+Ewwgen@users.noreply.github.com>
2022-08-11 18:20:03 +0300

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -4,3 +4,18 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
+
+
+names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
+name_leng = 0
+name_leng_list = []
+
+for i in names:
+  name_leng = len(i)
+  name_leng_list.append(name_leng )
+
+j = 0
+for i in name_leng_list:
+  j += 1
+  if i == max(name_leng_list):        
+    print(names[j-1])
