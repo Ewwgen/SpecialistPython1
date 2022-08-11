6f0f576be79f4ae4069483e23cdcbf190686d053
Ewwgen <110337565+Ewwgen@users.noreply.github.com>
2022-08-11 17:35:56 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,18 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+import random
+numbers = []
+n = int(input('Введите целое число: '))
+i = 0
+while i < n:
+  numbers.append(random.randint(0, 100))
+  i +=1
+print('Список состоит из чисел:' , *numbers)
+
+numbers_upd = []
+for i in numbers:
+  if round(i ** (1/2),0)-(i ** (1/2)) == 0:
+    numbers_upd.append(i ** (1/2))
+
+print('Новый список состоит из чисел:',  numbers_upd)
