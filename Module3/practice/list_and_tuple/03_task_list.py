94bf8f0e17369c14bf38db9a17002af7e25fd15b
Ewwgen <110337565+Ewwgen@users.noreply.github.com>
2022-08-12 18:36:09 +0300

    Update 03_task_list.py

--- Module3/practice/list_and_tuple/03_task_list.py
+++ Module3/practice/list_and_tuple/03_task_list.py
@@ -2,3 +2,16 @@
 # Вывести на экран сумму всех элементов.
 
 # TODO: your code here
+import random
+numbers = []
+n = int(input('Введите целое число: '))
+i = 0
+while i < n:
+  numbers.append(random.randint(-10, 10))
+  i +=1
+
+print('Список состоит из чисел: ', *numbers)
+summa = 0
+for i in numbers:
+  summa += i
+print('Сумма всех чисел списка равна ', summa)
