90f8858abcc02fec883626d72fb0a519f606e217
Ewwgen <110337565+Ewwgen@users.noreply.github.com>
2022-08-12 23:57:44 +0300

    Update 07_task_tuple.py

--- Module3/practice/list_and_tuple/07_task_tuple.py
+++ Module3/practice/list_and_tuple/07_task_tuple.py
@@ -3,3 +3,11 @@
 tup = (2, 4, 6, -4, 12, 0, 5)
 
 # TODO: your code here
+tup = (2, 4, 6, -4, 12, 0, 5)
+tup_list =[]
+for i in tup:
+    tup_list.append(i)
+
+tup_list.sort()
+
+print(tup_list[-1])
