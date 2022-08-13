a393fe6a91cac9a43ad30fe27846c9bc52934083
Ewwgen <110337565+Ewwgen@users.noreply.github.com>
2022-08-13 23:56:04 +0300

    Update 01_task_func.py

--- Module4/practice/funcs_part1/01_task_func.py
+++ Module4/practice/funcs_part1/01_task_func.py
@@ -4,7 +4,13 @@ def max4(n1, n2, n3, n4):
     # TODO: your code here
     pass
 
+def max4(n1, n2, n3, n4):
+    list_num = [n1, n2, n3, n4]
+    list_num.sort(reverse=True)
+    print(list_num[0])
+    pass
 
+max4(1,2,3,4)
 # Тестируем функцию
 print(max4(5, 6, 12, 7))
 print(max4(-10, -12, -4, -9))
