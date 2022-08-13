9ac00bf7dbc08e2a5e45d607e0b212109f98e639
Ewwgen <110337565+Ewwgen@users.noreply.github.com>
2022-08-13 23:30:04 +0300

    Update 03_hw_func.py

--- Module4/home_work/03_hw_func.py
+++ Module4/home_work/03_hw_func.py
@@ -2,3 +2,26 @@
 # Находится ли одна окружность целиком внутри другой
 
 # TODO: your code here
+def circles (x1,y1,r1,x2,y2,r2):
+ # ищем расстояние между двумя точками как гипотенузу треугольника
+    abx = 0
+    if x1 > x2:
+        abx = x1 - x2
+    else:
+        abx = x2 - x1
+    #print(abx)
+
+    aby = 0
+    if y1 > y2:
+        aby = y1 - y2
+    else:
+        aby = y2 - y1
+    #print(aby)
+    ab = (abx**2+aby**2)**(1/2)
+
+    if ab + r1 <= r2:
+        print('Входит в окружность')
+    else:
+        print('Не входит в окружность')
+
+circles(6,4,2,5,4,3)
