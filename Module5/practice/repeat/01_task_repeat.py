847905ba8e73e0a93a41cb58064bd75878588cdb
Ewwgen <110337565+Ewwgen@users.noreply.github.com>
2022-08-14 12:49:53 +0300

    Update 01_task_repeat.py

--- Module5/practice/repeat/01_task_repeat.py
+++ Module5/practice/repeat/01_task_repeat.py
@@ -2,6 +2,14 @@
 # произвольными целыми числами в заданном диапазоне.
 # , где size - размер генерируемого списка c элементами в диапазоне от of до to.
 
+import random
 def gen_list(size, of, to):
+    list = []
+    for i in range (size):
+        list.append(random.randint(of, to))
+    return list
     pass
+
+
+print(gen_list(5,1,6))
 
