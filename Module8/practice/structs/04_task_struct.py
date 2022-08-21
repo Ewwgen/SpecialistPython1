89a822c155f10a834b7af88c1f90eea01c53b12a
Ewwgen <110337565+Ewwgen@users.noreply.github.com>
2022-08-21 16:37:23 +0300

    Update 04_task_struct.py

--- Module8/practice/structs/04_task_struct.py
+++ Module8/practice/structs/04_task_struct.py
@@ -2,3 +2,11 @@
 dictionary_1 = {'a': 300, 'b': 400}
 dictionary_2 = {'c': 500, 'd': 600}
 # Объедините их в один
+
+for key, value in dictionary_2.items():
+    dictionary_1[key]=value
+
+print(dictionary_1)
+
+----------
+dictionary_1.update(dictionary_2)
