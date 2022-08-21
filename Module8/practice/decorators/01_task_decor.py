d2042d7cd185bf763ec05004ecd093c5192cc8e0
Ewwgen <110337565+Ewwgen@users.noreply.github.com>
2022-08-21 14:42:59 +0300

    Update 01_task_decor.py

--- Module8/practice/decorators/01_task_decor.py
+++ Module8/practice/decorators/01_task_decor.py
@@ -7,3 +7,24 @@
 # *23*
 # ****
 # (кол-во звездочек зависит от длины возвращаемого значения)
+def decor(func):
+    def wrapper():
+        result = func()
+        for i in range(len(result) + 2):
+            print("*", end="")
+        print()
+        print("*", end="")
+        print(result, end="")
+        print("*")
+        for i in range(len(result) + 2):
+            print("*", end="")
+
+    return wrapper
+
+
+@decor
+def privet():
+    return "Privet"
+
+
+privet()
