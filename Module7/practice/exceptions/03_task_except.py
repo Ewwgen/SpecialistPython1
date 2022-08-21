36c64f0ee939ba38c46dc204faffd3d1c3c50fcc
Ewwgen <110337565+Ewwgen@users.noreply.github.com>
2022-08-21 12:56:48 +0300

    Update 03_task_except.py

--- Module7/practice/exceptions/03_task_except.py
+++ Module7/practice/exceptions/03_task_except.py
@@ -5,3 +5,21 @@
 
 # При решении задачи требуется учесть формат входных данных.
 # Если входные данные некорректные, сообщить об этом.
+stroka= input('Введите 5 целых чисел через пробел')
+chisla =[]
+chisla_sorted =[]
+try:
+    for i in stroka.split():
+        chisla.append(int(i))
+    chisla_sorted.append(sorted(chisla))
+    #print(*chisla_sorted)
+
+    celye_chisla =[]
+    #print(chisla_sorted[0])
+    for i in chisla_sorted[0]:
+       if  int(i) >= 0:
+           celye_chisla.append(i)
+
+    print(sorted(celye_chisla)[0])
+except BaseException:
+    print('Ошибка')
