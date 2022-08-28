7bb423801c227d5a37631a6662701397aea7da2c
Ewwgen <110337565+Ewwgen@users.noreply.github.com>
2022-08-28 13:08:35 +0300

    Update 01_task_regexp.py

--- Module9/practice/regexp/01_task_regexp.py
+++ Module9/practice/regexp/01_task_regexp.py
@@ -6,4 +6,19 @@
 # 4) все слова начинающиеся на согласную букву
 # 5) все уникальные(без дубликатов) знаки препинания
 
-text = ...
+text = 'Дано произвольное предложение. Слова разделены? пробелами. Это предложение, содержит знаки препинания.'
+
+result1 = re.search(r'\w+\s', text)
+#print(result1)
+
+result2 = re.findall(r'(\w{2})\w+', text)
+#print(result2)
+
+result3 = re.findall(r'\b[АОИЕЁЭЫУЮЯ]\w+', text)
+#print(result3)
+
+result4 = re.findall(r'\b[^АОИЕЁЭЫУЮЯ]\w+', text)
+#print(result4)
+
+result5 = re.findall(r'\W(?!\S)', text)
+print(set(result5))
